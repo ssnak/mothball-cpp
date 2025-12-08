ae5fb464d766b3a75066a74fd00a58b5326a0c81
ssnak <sergregarias@gmail.com>
2025-12-07 22:47:51 -0500

    clean up main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
-#include <cstdio>
-
-#include "lexer.h"
 #include "parser.h"
-#include "player.h"
 
 int main() {
-    std::string input = "sprintjump 2 + 5.0 * 2 walk.s sprintjump sprintair";
+    std::string input =
+        "facing 30.3 stopjump walkair.s 0 sneak.s 4 stop stopjump outvz sprintair outz outvz sprintair 10 sprint 1 "
+        "outz 1 outx -0.4 | sprintjump 9 outz -0.6 outx 0.6";
     Scanner scanner(input);
-    CodeVisitor vis;
-    scanner.scan().accept(vis);
-    // std::string input =
-    //     "2315 + 531.21\n while x < 5 { x = 5 }\n -sj.wa 12\n if jumping == true {jumping = false} dal234h;6ajsdf90  "
-    //     "50 "
-    //     "* 2 - 3 + 5 / 8";
-    // Lexer lex(input);
-    // Token t;
-    // while (t.type != TokenType::EndOfFile) {
-    //     t = lex.next();
-    //     printf("%d %s\n", static_cast<int>(t.type), t.text.c_str());
-    // }
-    //
-    // Player player;
-    // player.inputs = "w";
-    // player.sprintjump(12);
-    // printf("%f %f\n", player.velocity.z, player.position.z);
+    CodeVisitor visitor;
+    scanner.scan().accept(visitor);
 }
